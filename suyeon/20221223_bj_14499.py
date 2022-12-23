e0692373914e19e746559fb6b578f1e10916bfaa
suyeon woo <woosean999@gmail.com>
2022-12-23 13:21:10 +0900

    [수연]14499 주사위 굴리기

---
+++ suyeon/20221223_bj_14499.py
@@ -0,0 +1,56 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def roll(dir):
+    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    if dir == 1: #동
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
+
+    elif dir == 2: #서
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+
+    elif dir == 3: #북
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
+
+    else:   #남
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+
+N,M,x,y,k = map(int, input().split())   # x,y = 시작점의 행,열
+
+arr = []
+
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+
+dr = [0, 0, -1, 1]
+dc = [1, -1, 0, 0]
+dice = [0, 0, 0, 0, 0, 0]
+
+mvs = list(map(int, input().split()))
+
+nr = x
+nc = y
+for i in mvs:
+    nr += dr[i-1]
+    nc += dc[i-1]
+
+    if nr < 0 or nr >= N or nc < 0 or nc >= M:
+        nr -= dr[i-1]
+        nc -= dc[i-1]
+        continue
+    if 0 <= nr < N and 0 <= nc < M:
+        roll(i)
+
+        #주사위를 굴렸을 때, 이동한 칸에 쓰여 있는 수가 0이면, 주사위의 바닥면에 쓰여 있는 수가 칸에 복사된다. 주사위 수 유지
+        if arr[nr][nc] == 0:
+            arr[nr][nc]= dice[-1]
+
+        #주사위를 굴렸을 때, 이동한 칸에 쓰여 있는 수가 1 ~ 9 이면 칸에 적힌 수가 주사위의 바닥면으로 복사되며, 칸에 쓰여 있는 수는 0이 된다.   
+        elif arr[nr][nc] > 0:
+            dice[-1] = arr[nr][nc]
+            arr[nr][nc] = 0
+
+        print(dice[0])
